c1a40aaa35bb0697e9093a3760038fc3af9f7d81
Adarsh Agrawal <adarshagrawal105@gmail.com>
2025-12-04 11:44:39 -0800

    wordle

--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,13 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void helper(
+    std::string current,
+    std::string floating,
+    std::set<std::string>& words,
+    const std::string& in, 
+    const std::set<std::string>& dict
+);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -21,8 +27,42 @@ std::set<std::string> wordle(
     const std::string& floating,
     const std::set<std::string>& dict)
 {
-    // Add your code here
-
+    std::string current = ""; 
+    std::set<std::string> words; 
+    helper(current, floating, words, in, dict); 
+    return words; 
 }
 
 // Define any helper functions here
+void helper(
+    std::string current,
+    std::string floating,
+    std::set<std::string>& words,
+    const std::string& in, 
+    const std::set<std::string>& dict)
+{
+    if (current.size() == in.size()){ // base case
+        if (floating.size() > 0){
+            return; 
+        }
+        if (dict.find(current) != dict.end()){
+            words.insert(current); 
+        }
+        return; 
+    }
+
+    if(in[current.size()] != '-'){
+        helper(current + in[current.size()], floating, words, in, dict);
+    }
+    else {
+        for (char c = 'a'; c <= 'z'; c++){
+            std::string floating2 = floating; 
+
+            if (floating.find(c) != string::npos){
+                floating2.erase(floating2.find(c), 1);
+            }
+
+            helper(current + c, floating2, words, in, dict);
+        }
+    }
+}
