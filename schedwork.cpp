da3951d9e70ae194cb6400c9566355a7733e2eea
Adarsh Agrawal <adarshagrawal105@gmail.com>
2025-12-04 15:11:35 -0800

    iteration 1 schedwork

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,15 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool solve(
+    const AvailabilityMatrix& avail, 
+    size_t dailyNeed, 
+    size_t maxShifts, 
+    DailySchedule& sched, 
+    std::vector<size_t>& shiftCounts, 
+    size_t day,
+    size_t slot
+);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -37,9 +45,52 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+    sched.resize(avail.size());    
+    for(int i = 0; i < avail.size(); i++){
+        sched[i].resize(dailyNeed);
+    }
+    std::vector<size_t> shiftCounts(avail[0].size());
 
-
-
-
+    return solve(avail, dailyNeed, maxShifts, sched, shiftCounts, 0, 0);
 }
 
+bool solve(
+    const AvailabilityMatrix& avail, 
+    size_t dailyNeed, 
+    size_t maxShifts, 
+    DailySchedule& sched, 
+    std::vector<size_t>& shiftCounts, 
+    size_t day,
+    size_t slot
+)
+{
+    if (day == sched.size()){ // finished scheduling
+        return true; 
+    }
+    else if (slot == dailyNeed){ // finished that day
+        return solve(avail, dailyNeed, maxShifts, sched, shiftCounts, day + 1, 0);
+    }
+    else {
+        int start; 
+        if(slot == 0){
+            start = 0;
+        }
+        else {
+            start = sched[day][slot - 1] + 1;
+        }
+        for (int i = start; i < avail[day].size(); i++){
+            if (avail[day][i] == 1 && shiftCounts[i] < maxShifts){
+                sched[day][slot] = i; 
+                shiftCounts[i] += 1; 
+                if (solve(avail, dailyNeed, maxShifts, sched, shiftCounts, day, slot + 1)){
+                    return true; 
+                }
+                else {
+                    shiftCounts[i] -= 1; 
+                }
+            }
+        }
+    }
+
+    return false; 
+}
